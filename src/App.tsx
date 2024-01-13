6b2349111c875677bdac29a6eebcc6c9d8e55bbe
Ibrahim Mammadov <zxyctn@gmail.com>
2024-01-13 11:34:09 +0100

    Initial setup with auth

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './index.css';
+
+import React, { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import type { Session } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  `${process.env.VITE_SUPABASE_URL}`,
+  `${process.env.VITE_SUPABASE_API_KEY}`
+);
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return <div>Logged in!</div>;
+  }
+}
