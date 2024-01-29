60a516d41603647f93f52498121d44934b1121f6
Ibrahim Mammadov <zxyctn@gmail.com>
2024-01-30 00:34:59 +0100

    Config themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,38 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: '#0093FF',
+          secondary: '#FFFFFF',
+          accent: '#BD00FF',
+          neutral: '#484848',
+          'base-100': '#0B0B0B',
+          info: '#00E0FF',
+          success: '#42FF00',
+          warning: '#FFE500',
+          error: '#FF0000',
+          '--rounded-btn': '0',
+          '--rounded-box': '0',
+        },
+
+        light: {
+          primary: '#0093FF',
+          secondary: '#000000',
+          accent: '#BD00FF',
+          neutral: '#484848',
+          'base-100': '#FFFFFF',
+          info: '#00E0FF',
+          success: '#42FF00',
+          warning: '#FFE500',
+          error: '#FF0000',
+          '--rounded-btn': '0',
+          '--rounded-box': '0',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
 };
