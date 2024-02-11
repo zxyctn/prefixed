e51a6b681326165a8d19f02f80ea023912d6546d
Ibrahim Mammadov <zxyctn@gmail.com>
2024-02-11 11:47:40 +0100

    Add modals

---
+++ src/components/Confirm.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef } from 'react';
+import Button from './Button';
+
+const Confirm = ({
+  id,
+  children,
+  title,
+  confirmButtonText,
+  cancelButtonText,
+  onConfirm = () => {},
+  onCancel = () => {},
+}) => {
+  const dialog = useRef(null);
+
+  const handleConfirm = (e, result) => {
+    e.preventDefault();
+
+    if (dialog.current) {
+      (dialog.current as HTMLDialogElement).close();
+    }
+
+    result ? onConfirm() : onCancel();
+  };
+
+  useEffect(() => {
+    if (!dialog.current) {
+      return;
+    }
+    (dialog.current as HTMLDialogElement).addEventListener(
+      'cancel',
+      (event) => {
+        event.preventDefault();
+      }
+    );
+  }, [dialog]);
+
+  return (
+    <dialog
+      id={id}
+      className='modal modal-bottom sm:modal-middle'
+      data-keyboard='false'
+      ref={dialog}
+    >
+      <div className='modal-box relative'>
+        <h1 className='bg-neutral absolute top-0 left-0 separated roboto-regular uppercase text-center p-3 w-full'>
+          {title}
+        </h1>
+        <div className='mt-12'>
+          {children}
+          <div className='modal-action w-full gap-5'>
+            <form
+              action='dialog'
+              className='grid gap-5 place-content-stretch place-items-stretch place-self-stretch w-full'
+            >
+              <Button
+                className='btn-primary text-white'
+                onClick={(e) => {
+                  handleConfirm(e, true);
+                }}
+              >
+                {confirmButtonText}
+              </Button>
+              <Button
+                className='btn-secondary'
+                onClick={(e) => {
+                  handleConfirm(e, false);
+                }}
+              >
+                {cancelButtonText}
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default Confirm;
---
+++ src/components/Modal.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Modal = ({ id, children }) => {
+  return (
+    <dialog id={id} className='modal modal-bottom sm:modal-middle'>
+      <div className='modal-box'>
+        <form method='dialog'>
+          <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
+            ✕
+          </button>
+        </form>
+        {...children}
+      </div>
+      <form method='dialog' className='modal-backdrop'>
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+};
+
+export default Modal;
