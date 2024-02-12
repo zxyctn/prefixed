d82ca6ca396ab1d0126dd48db59279d9012a7fe1
Ibrahim Mammadov <zxyctn@gmail.com>
2024-02-12 12:08:16 +0100

    Update Button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Button = ({ className, children, onClick }: ButtonProps) => {
+import type { ButtonProps } from '../types';
+
+const Button = ({
+  className,
+  children,
+  onClick,
+  type = 'button',
+}: ButtonProps) => {
   return (
     <button
-      className={`btn roboto-regular separated uppercase text-2xl p-8 items-center content-center ${className}`}
+      className={`btn roboto-medium separated-min md:separated uppercase text-2xl p-8 h-[70px] md:h-[80px] items-center content-center ${className}`}
       onClick={onClick}
+      type={type}
     >
       {children}
     </button>
