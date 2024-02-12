b1d3e3e7dcaffbe3257bfc7428d8d0b99b63c01a
Ibrahim Mammadov <zxyctn@gmail.com>
2024-02-12 12:09:13 +0100

    Add props types

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,27 @@
-type ButtonProps = {
-	children: React.ReactNode;
-	onClick?: any;
-	className?: string;
+import { ReactNode } from 'react';
+
+export type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: any;
+  className?: string;
+  type?: 'button' | 'submit' | 'reset';
 };
 
-type ConfirmModalProps = {
-	id: string;
-	title: string;
-	children: React.ReactNode;
-	onConfirm: () => void;
-	onCancel: () => void;
-	confirmButtonText?: string;
-	cancelButtonText?: string;
+export type InputFieldProps = {
+  children?: ReactNode;
+  vertical?: boolean;
+  className?: string;
+  type?: string;
+  required?: boolean;
+  onChange?: (e) => void;
+};
+
+export type ConfirmModalProps = {
+  id: string;
+  title: string;
+  children: React.ReactNode;
+  onConfirm: () => void;
+  onCancel: () => void;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
 };
