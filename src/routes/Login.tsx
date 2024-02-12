e9dd750bf073fd29e0d4db7cf62208763d4c743f
Ibrahim Mammadov <zxyctn@gmail.com>
2024-02-12 12:08:38 +0100

    Add Login route

---
+++ src/routes/Login.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import { User, Key } from 'react-feather';
+import type { SupabaseClient } from '@supabase/supabase-js';
+
+import InputField from '../components/InputField';
+import Button from '../components/Button';
+
+const Login = () => {
+  const supabase: SupabaseClient = useOutletContext();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className='w-full h-full p-6 m-auto flex justify-center items-center'>
+      <form className='grid gap-6 max-w-[400px]' onSubmit={submitHandler}>
+        <InputField type='email' required={true} onChange={onEmailChange}>
+          <User />
+        </InputField>
+
+        <InputField type='password' required={true} onChange={onPasswordChange}>
+          <Key />
+        </InputField>
+
+        <Button className='btn-primary' type='submit'>
+          Login
+        </Button>
+        <Button className='btn-secondary'>Register</Button>
+
+        <a href='' className='uppercase separated-min md:separated font-medium'>
+          Forgot password?
+        </a>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
