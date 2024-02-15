e4204f12c09ab11736c619044b54e675c3802c51
Ibrahim Mammadov <zxyctn@gmail.com>
2024-02-15 10:22:41 +0100

    Add Join UI

--- src/routes/Join.tsx
+++ src/routes/Join.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import InputField from '../components/InputField';
+import { Key } from 'react-feather';
+import Button from '../components/Button';
 
-const Join = ({setPage}) => {
-  setPage('Join');
-
-  return <div>Join</div>;
+const Join = () => {
+  return (
+    <div className='w-full h-full grid place-items-center place-content-center gap-3 md:gap-6'>
+      <InputField>
+        <Key />
+      </InputField>
+      <Button className='btn-primary w-full'>Join</Button>
+    </div>
+  );
 };
 
 export default Join;
