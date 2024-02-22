e23ef9498ab87c4d17eb65ab6b59db38641bcba2
Ibrahim Mammadov <zxyctn@gmail.com>
2024-02-23 00:15:53 +0100

    Display game turns with user avatars, let insert

--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,147 +1,133 @@
-import { SupabaseClient } from '@supabase/supabase-js';
 import React, { useEffect, useState } from 'react';
 import { Send } from 'react-feather';
+import { SupabaseClient } from '@supabase/supabase-js';
 import { useOutletContext, useParams } from 'react-router-dom';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+
+import { currentGame, currentUser, gamePlayers } from '../stores';
+import { getAvatars, getGame, getPlayers, getTurns } from '../shared';
 
 const Game = () => {
   const id = useParams<{ id: string }>().id;
   const supabase: SupabaseClient = useOutletContext();
 
-  const [lang, setLang] = useState<string>('');
-  const [prefix, setPrefix] = useState<string>('');
+  const [game, setGame] = useRecoilState(currentGame);
+  const [player] = useRecoilState(currentUser);
+  const setPlayers = useSetRecoilState(gamePlayers);
+  const [avatars, setAvatars] = useState<{ [key: string]: string }>({});
+
   const [word, setWord] = useState<string>('');
   const [playerPoints, setPlayerPoints] = useState<number>(0);
-  const [player, setPlayer] = useState<string>('');
   const [turns, setTurns] = useState<any[]>([]);
-  const [avatar, setAvatar] = useState<string>('#000');
 
   useEffect(() => {
-    supabase
-      .from('game')
-      .select('*')
-      .eq('id', id)
-      .then(({ data, error }) => {
-        if (error) {
-          console.error(error);
-        } else {
-          setPrefix(data[0].prefix);
-          setWord(data[0].prefix);
-          setLang(data[0].lang);
-          setPlayerPoints(0);
-          turnsHandler();
-        }
+    if (id) {
+      getGame(id, supabase).then(({ data, error }) => {
+        setGame(data[0]);
+        setWord(data[0].prefix);
       });
+
+      getPlayers(id, supabase).then(({ data, error }) => {
+        setPlayers(
+          data ? data.map((player) => ({ id: player.player_id })) : []
+        );
+      });
+
+      getAvatars(id, supabase).then(({ data, error }) => {
+        data?.forEach((player) => {
+          avatars[player.player_id] = player.colors.hex;
+        });
+      });
+
+      getTurns(id, supabase).then(({ data, error }) => {
+        setTurns(data?.reverse() || []);
+      });
+
+      supabase
+        .channel(`game=${id}`)
+        .on(
+          'postgres_changes',
+          {
+            event: 'INSERT',
+            schema: 'public',
+            table: 'game_turns',
+            filter: `game_id=eq.${id}`,
+          },
+          (payload) => {
+            setTurns((old) =>
+              [
+                ...old,
+                {
+                  word: payload.new.word,
+                  player_id: payload.new.player_id,
+                  id: payload.new.id,
+                },
+              ].slice(-10)
+            );
+          }
+        )
+        .subscribe();
+    }
   }, [id, supabase]);
-
-  useEffect(() => {
-    const setUser = async () => {
-      const { data, error } = await supabase.auth.getUser();
-      if (!error) {
-        setPlayer(data.user.id);
-
-        console.log(data.user.id);
-
-        const { data: colorData, error: colorError } = await supabase
-          .from('player_colors')
-          .select('color_id')
-          .eq('player_id', data.user.id)
-          .eq('game_id', id);
-
-        console.log(colorData);
-
-        const { data: avatarData, error: avatarError } = await supabase
-          .from('colors')
-          .select('*')
-          .eq('id', colorData![0].color_id);
-
-        setAvatar(avatarData![0].hex);
-      }
-    };
-    setUser();
-  }, [supabase]);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (
-      e.target.value.length > prefix.length &&
-      e.target.value.startsWith(prefix)
+      game &&
+      e.target.value.length > game.prefix.length &&
+      e.target.value.startsWith(game.prefix)
     ) {
       setWord(e.target.value);
     } else {
-      setWord(prefix);
+      setWord(game?.prefix || '');
     }
   };
-
-  const turnsHandler = async () => {
-    const { data, error } = await supabase
-      .from('game_turns')
-      .select('*')
-      .eq('game_id', id);
-
-    if (error) {
-      console.error(error);
-    } else {
-      setTurns(data.slice(-10));
-    }
-  };
-
-  supabase
-    .channel(`game=${id}`)
-    .on(
-      'postgres_changes',
-      {
-        event: 'INSERT',
-        schema: 'public',
-        table: 'game_turns',
-        filter: `game_id=eq.${id}`,
-      },
-      turnsHandler
-    )
-    .subscribe();
 
   const keystrokeHandler = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      const { data, error } = await supabase
-        .from(lang.toLowerCase())
+      supabase
+        .from(game!.lang.toLowerCase())
         .select('*')
-        .ilike('word', word);
-
-      if (error) {
-        console.error(error);
-      } else {
-        if (data.length > 0) {
-          await supabase
+        .ilike('word', word)
+        .then(({ data, error }) => {
+          supabase
             .from('game_players')
             .update({ points: playerPoints + 1 })
-            .eq('player_id', player);
-          setPlayerPoints(playerPoints + 1);
-
-          await supabase.from('game_dict').insert({
-            word: word,
-            pre_1: word.charAt(0),
-            pre_2: word.substring(0, 2),
-            pre_3: word.substring(0, 3),
-            pre_4: word.substring(0, 4),
-            lang: lang,
-            game: id,
-          });
-
-          await supabase.from('game_turns').insert({
-            player_id: player,
-            word: word,
-            game_id: id,
-            existent: true,
-            repeated: false,
-            accepted: true,
-          });
-        } else {
-          console.log('Word does not exist');
-        }
-      }
+            .eq('player_id', player?.id)
+            .then(() => {
+              setPlayerPoints(playerPoints + 1);
+              supabase
+                .from('game_dict')
+                .insert({
+                  word: word,
+                  pre_1: word.charAt(0),
+                  pre_2: word.substring(0, 2),
+                  pre_3: word.substring(0, 3),
+                  pre_4: word.substring(0, 4),
+                  lang: game!.lang,
+                  game: id,
+                })
+                .then(() => {
+                  supabase
+                    .from('game_turns')
+                    .insert({
+                      player_id: player?.id,
+                      word: word,
+                      game_id: id,
+                      existent: true,
+                      repeated: false,
+                      accepted: true,
+                    })
+                    .then(({ data, error }) => {
+                      setWord(game?.prefix || '');
+                    });
+                });
+            });
+        });
 
       // TODO: Show modal to start a poll for the word's existence
     } else if (
-      ((!/^[a-zA-Z]$/i.test(e.key) && lang === 'en') ||
-        (!/^[a-zA-ZƏəĞğİiÖöÜüÇçŞş]$/i.test(e.key) && lang === 'az')) &&
+      ((!/^[a-zA-Z]$/i.test(e.key) && game!.lang === 'en') ||
+        (!/^[a-zA-ZƏəĞğİiÖöÜüÇçŞş]$/i.test(e.key) && game!.lang === 'az')) &&
       e.key !== 'Backspace'
     ) {
       e.preventDefault();
@@ -150,19 +136,30 @@ const Game = () => {
 
   return (
     <div className='h-full w-full p-4 flex flex-col'>
-      <span className='uppercase separated-min flex justify-center w-full text-lg'>
-        {prefix}
+      <span className='uppercase separated-min flex justify-center w-full text-2xl pb-4'>
+        {game?.prefix}
       </span>
-      <div className='grow'>
-        {turns.map((turn) => (
-          <div key={turn.word} className='grid gap-2'>
-            <div>user: {turn.player_id}</div>
-            <div>word: {turn.word}</div>
-          </div>
-        ))}
+      <div className='grow flex flex-col gap-5'>
+        {turns &&
+          turns.map((turn) => (
+            <div key={turn.id} className='flex gap-3 items-center'>
+              <div
+                className='w-3 h-3'
+                style={{
+                  background: avatars[turn.player_id] || '#fff',
+                }}
+              ></div>
+              <div className='separated uppercase text-2xl'>{turn.word}</div>
+            </div>
+          ))}
       </div>
       <div className='relative bg-neutral flex items-center px-3'>
-        <div className='w-3 h-3' style={{ background: avatar }}></div>
+        <div
+          className='w-3 h-3'
+          style={{
+            background: player ? avatars[player.id] : '#fff',
+          }}
+        ></div>
         <input
           type='text'
           className='p-3 bg-transparent w-10/12 uppercase separated-min'
--- src/types.ts
+++ src/types.ts
@@ -28,3 +28,17 @@ export type ConfirmModalProps = {
   confirmButtonText?: string;
   cancelButtonText?: string;
 };
+
+export type GameType = {
+  created_at: string;
+  creator_id: string;
+  id: number;
+  lang: 'az' | 'en';
+  number_of_players: number;
+  password: string | null;
+  prefix: string;
+  state: 'not_started' | 'in_progress' | 'not_ready';
+  turn: number | null;
+  turn_duration: number | null;
+  unique_id: string;
+};
