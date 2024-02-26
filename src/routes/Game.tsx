b790666086385f0ac7cdfeb9fd3d6db2799b167e
Ibrahim Mammadov <zxyctn@gmail.com>
2024-02-26 12:38:33 +0100

    Add "typing..." indicator

--- src/routes/Game.tsx
+++ src/routes/Game.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Send } from 'react-feather';
-import { SupabaseClient } from '@supabase/supabase-js';
+import { RealtimeChannel, SupabaseClient } from '@supabase/supabase-js';
 import { useOutletContext, useParams } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 import { currentGame, currentUser, gamePlayers } from '../stores';
-import { getAvatars, getGame, getPlayers, getTurns } from '../shared';
+import { getGame } from '../shared';
 
 const Game = () => {
   const id = useParams<{ id: string }>().id;
@@ -15,6 +15,11 @@ const Game = () => {
   const player = useRecoilValue(currentUser);
   const [players, setPlayers] = useRecoilState(gamePlayers);
   const [avatars, setAvatars] = useState<{ [key: string]: string }>({});
+  const [channel, setChannel] = useState<RealtimeChannel>();
+  const [isTyping, setIsTyping] = useState<{
+    value: boolean;
+    player_id: string;
+  }>({ value: false, player_id: '' });
 
   const [word, setWord] = useState<string>('');
   const [playerPoints, setPlayerPoints] = useState<number>(0);
@@ -92,43 +97,53 @@ const Game = () => {
   }, [game, supabase]);
 
   useEffect(() => {
-    supabase
-      .channel(`game=${id}`)
-      .on(
-        'postgres_changes',
-        {
-          event: 'INSERT',
-          schema: 'public',
-          table: 'game_turns',
-          filter: `game_id=eq.${id}`,
-        },
-        (payload) => {
-          setTurns((old) =>
-            [
-              ...old,
-              {
-                word: payload.new.word,
-                player_id: payload.new.player_id,
-                id: payload.new.id,
-              },
-            ].slice(-10)
-          );
-        }
-      )
-      .on(
-        'postgres_changes',
-        {
-          event: 'UPDATE',
-          schema: 'public',
-          table: 'game',
-          filter: `id=eq.${id}`,
-        },
-        (payload) => {
-          setIsTurn(payload.new.turn === order);
-        }
-      )
-      .subscribe();
-  }, [order]);
+    if (supabase) {
+      const supabaseChannel = supabase.channel(`game=${id}`);
+      setChannel(supabaseChannel);
+
+      supabaseChannel
+        .on(
+          'postgres_changes',
+          {
+            event: 'INSERT',
+            schema: 'public',
+            table: 'game_turns',
+            filter: `game_id=eq.${id}`,
+          },
+          (payload) => {
+            setTurns((old) =>
+              [
+                ...old,
+                {
+                  word: payload.new.word,
+                  player_id: payload.new.player_id,
+                  id: payload.new.id,
+                },
+              ].slice(-10)
+            );
+          }
+        )
+        .on(
+          'postgres_changes',
+          {
+            event: 'UPDATE',
+            schema: 'public',
+            table: 'game',
+            filter: `id=eq.${id}`,
+          },
+          (payload) => {
+            setIsTurn(payload.new.turn === order);
+          }
+        )
+        .on('broadcast', { event: 'typing' }, ({ payload }) => {
+          setIsTyping({ value: true, player_id: payload.player_id });
+          setTimeout(() => {
+            setIsTyping({ value: false, player_id: '' });
+          }, 1000);
+        })
+        .subscribe();
+    }
+  }, [order, supabase]);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (
@@ -186,10 +201,20 @@ const Game = () => {
 
       // TODO: Show modal to start a poll for the word's existence
     } else if (
-      ((!/^[a-zA-Z]$/i.test(e.key) && game!.lang === 'en') ||
-        (!/^[a-zA-ZƏəĞğİiÖöÜüÇçŞş]$/i.test(e.key) && game!.lang === 'az')) &&
-      e.key !== 'Backspace'
+      (/^[a-zA-Z]$/i.test(e.key) && game!.lang === 'en') ||
+      (/^[a-zA-ZƏəĞğİiÖöÜüÇçŞş]$/i.test(e.key) && game!.lang === 'az') ||
+      e.key === 'Backspace'
     ) {
+      if (channel && player) {
+        channel.send({
+          type: 'broadcast',
+          event: 'typing',
+          payload: {
+            player_id: player.id,
+          },
+        });
+      }
+    } else {
       e.preventDefault();
     }
   };
@@ -211,6 +236,17 @@ const Game = () => {
             <div className='separated uppercase text-2xl'>{turn.word}</div>
           </div>
         ))}
+        {isTyping.value && (
+          <div className='flex gap-3 items-center animate-pulse delay-300'>
+            <div
+              className='w-3 h-3'
+              style={{
+                background: avatars[isTyping.player_id],
+              }}
+            ></div>
+            <span className='loading loading-dots loading-lg'></span>
+          </div>
+        )}
       </div>
       {!isTurn && (
         <span
