2f8dcb34d3f779869ebc509bc86fdfb94c5d7e3e
Ibrahim Mammadov <zxyctn@gmail.com>
2024-03-15 19:48:33 +0100

    Fix typescript bug related to children

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Modal = ({ id, children }) => {
   return (
-    <dialog id={id} className='modal modal-bottom sm:modal-middle'>
+    <dialog id={id} className='modal sm:modal-middle'>
       <div className='modal-box'>
         <form method='dialog'>
           <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
             ✕
           </button>
         </form>
-        {...children}
+        {children}
       </div>
       <form method='dialog' className='modal-backdrop'>
         <button>close</button>
