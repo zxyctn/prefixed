e8e56bb1f69f9a75a5faf126e042d86ac52406e0
Ibrahim Mammadov <zxyctn@gmail.com>
2024-03-20 11:33:10 +0100

    Add PWA capabilities and required assets and config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,57 @@
 import dotenv from 'dotenv';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,ts,tsx,css,html,ico,png,svg}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'prefixed',
+        short_name: 'prefixed',
+        description: 'prefixed',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        scope: '/prefixed/',
+        start_url: '/prefixed/',
+        display: 'standalone',
+        background_color: '#000000',
+        orientation: 'portrait',
+      },
+    }),
+  ],
   base: '/prefixed/',
   define: {
     'process.env': process.env,
