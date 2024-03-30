c7a0a36d2552cf5974139ff17c9b5155b21fe243
Ibrahim Mammadov <zxyctn@gmail.com>
2024-03-30 23:13:24 +0100

    Center separated text

--- src/components/Separated.tsx
+++ src/components/Separated.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Separated = ({ content, className = '' }) => {
   return (
-    <div className='m-auto uppercase'>
-      <span className={className.length ? className : 'separated'}>
-        {content && content.slice(0, -1)}
-      </span>
-      <span>{content && content[content.length - 1]}</span>
+    <div className='flex justify-center items-center'>
+      <div className='uppercase text-center'>
+        <span className={` ${className.length ? className : 'separated'}`}>
+          {content && content.slice(0, -1)}
+        </span>
+        <span>{content && content[content.length - 1]}</span>
+      </div>
     </div>
   );
 };
